159d54c576e91e2471de8bf9e24765e119b3fe20
Judah Lamm <Judah.Lamm@gmail.com>
2015-06-18 13:52:15 -1000

    final solution w/ working test

--- bubble-sorter.js
+++ bubble-sorter.js
@@ -1,25 +1,20 @@
-
-
-
 function _bubbleSorter(sortMe){
 
-    if(!Array.isArray(sortMe)){
-      throw new TypeError('Your sortMe needs to be an array');
-    }
-
-    for(var j = 0; j < sortMe.length; j++){
-      for(var i = 0; i < sortMe.length; i++){
-        if(sortMe[i] > sortMe[i+1]){
-          var tempNum = sortMe[i];
-          sortMe[i] = sortMe[i+1];
-          sortMe[i + 1] = tempNum;
-        }
-      }
-    }
-    return sortMe;
+  if(!Array.isArray(sortMe)){
+    throw new TypeError('Your sortMe needs to be an array');
   }
 
-
+  for(var j = 0; j < sortMe.length; j++){
+    for(var i = 0; i < sortMe.length; i++){
+      if(sortMe[i] > sortMe[i+1]){
+        var tempNum = sortMe[i];
+        sortMe[i] = sortMe[i+1];
+        sortMe[i + 1] = tempNum;
+      }
+    }
+  }
+  return sortMe;
+}
 
 module.exports = {
 
--- test/bubble-sorter-spec.js
+++ test/bubble-sorter-spec.js
@@ -3,25 +3,20 @@ var chai = require('chai');
 var should = chai.should();
 var expect = chai.expect;
 
-var bubbleSorter = require('../bubble-sorter.js');
+var bs = require('../bubble-sorter.js');
 
-  console.log(bubbleSorter);
+
 
 describe('Bubble Sorter', function () {
   describe('Bubble Function', function () {
 
     it('should be a function', function () {
-      bubbleSorter.should.be.a.function;
+      bs.should.be.a.function;
     });
 
-    it('the input should be an array', function () {
-
-      bubbleSorter([1,3]).should.equal.true;
-
+    it('depp equl', function () {
+      bs.bubbleSorter([3,2,1]).should.deep.equal([1,2,3])
     });
-
-
-
 
   });
 });
