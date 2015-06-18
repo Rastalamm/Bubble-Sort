fa82e69e38170b8ca80c266a80c9f178ceffc0f5
Judah Lamm <Judah.Lamm@gmail.com>
2015-06-18 12:09:11 -1000

    working solution

--- bubble-sorter.js
+++ bubble-sorter.js
@@ -6,25 +6,17 @@ function bubbleSorter(input){
     throw new TypeError('Your input needs to be an array');
   }
 
- var theLength = input.length;
- var theNum;
-
+ for(var j = 0; j < input.length; j++){
     for(var i = 0; i < input.length; i++){
-
-      if(input[i + 1] === undefined){
-          console.log(input);
-          return input;
-      }else if(input[i] > input[i+1]){
-          var tempNum = input[i];
-          input[i] = input[i+1];
-          input[i + 1] = tempNum;
-      console.log(input);
+      if(input[i] > input[i+1]){
+        var tempNum = input[i];
+        input[i] = input[i+1];
+        input[i + 1] = tempNum;
       }
     }
+  }
 
-
-
-
+ return input;
 
 }
 
