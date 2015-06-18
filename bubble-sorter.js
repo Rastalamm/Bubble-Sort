71593c8e14eb024de2924cb5c2b168148c281f52
Judah Lamm <Judah.Lamm@gmail.com>
2015-06-18 10:17:45 -1000

    notes about workflow

--- bubble-sorter.js
+++ bubble-sorter.js
@@ -1,4 +1,61 @@
-function bubbleSorter(){
+
+
+function bubbleSorter(input){
+
+  if(!Array.isArray(input)){
+    throw new TypeError('Your input needs to be an array');
+  }
+
+tempArr = [];
+
+
+
+  input.forEach(function(currelement, index, arr){
+    //validation of the last index
+    if(typeof index[i+1] !== 'number'){
+      //at the end, jsut take the last value and recurse here starting from the next index.. 0,1,2,3 etc..
+      return false
+    }
+
+
+    if(currelement < currelement[index+1]){
+      //the first number is smaller
+      //don't switch the order, move on tp the next index/recurse?
+    }
+    else{
+      //here current ele is smaller than the next ele
+      //test for the above line
+
+      //switch the elements place
+      //splice out all from before,
+
+    }
+
+
+
+  })
+
+
+
+
+
+
+  for(var i = 0; i < input.length; i++){
+
+
+    if(input[i] < input[i+1]){
+      //do nothing and move on to the next nums
+    }
+    else{
+
+    }
+  };
+
+
+
+
+
 
 }
 
+module.exports = bubbleSorter;
--- test/bubble-sorter-spec.js
+++ test/bubble-sorter-spec.js
@@ -5,12 +5,22 @@ var expect = chai.expect;
 
 var bubbleSorter = require('../bubble-sorter.js');
 
+  console.log(bubbleSorter);
+
 describe('Bubble Sorter', function () {
-  describe('Buble Function', function () {
+  describe('Bubble Function', function () {
 
     it('should be a function', function () {
       bubbleSorter.should.be.a.function;
     });
+
+    it('the input should be an array', function () {
+
+      bubbleSorter([1,3]).should.equal.true;
+
+    });
+
+
 
   });
 });
